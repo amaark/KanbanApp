5a797d301901d386ca8eed286d6ca26110e725fe
Amaar <amaar.khan02@gmail.com>
2021-12-06 21:01:07 +0000

    Add db seeds and update board validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+boards = Board.create([board_title: "My Board", board_description: "Add anything you like!"])
